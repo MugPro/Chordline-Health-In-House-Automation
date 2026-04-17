37949ce8f1d7744563c4d3a1b96772b40b6a4850
Matas Backevicius <matas.backevicius+chord@chordline.com>
2026-04-16 18:51:28 -0700

    reduced the timout from 360000 to 270000

--- playwright.config.js
+++ playwright.config.js
@@ -4,7 +4,7 @@ import { env as qawolf2Env } from './environments/qawolf2.env.js';
 
 export default defineConfig({
     //timeout: 360000,
-    timeout: 204000,
+    timeout: 270000,
     retries: 1,
     workers: 1, // ← run tests one at a time
     reporter: [['list'], ['html', { outputFolder: 'playwright-report' }]],

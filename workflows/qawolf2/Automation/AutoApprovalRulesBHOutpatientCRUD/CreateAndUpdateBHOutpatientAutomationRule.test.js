6be25614604f1dc924026aed31463b9c27fba563
Matas Backevicius <matas.backevicius+chord@chordline.com>
2026-04-27 15:03:51 -0700

    updated tests due to flakiness

--- workflows/qawolf2/Automation/AutoApprovalRulesBHOutpatientCRUD/CreateAndUpdateBHOutpatientAutomationRule.test.js
+++ workflows/qawolf2/Automation/AutoApprovalRulesBHOutpatientCRUD/CreateAndUpdateBHOutpatientAutomationRule.test.js
@@ -10,7 +10,7 @@ test('Create BH Outpatient Automation Rule', async () => {
     // Arrange
     //--------------------------------
     //const loginID = `BHOutpatientAutomationRule`;
-    const loginID = `ValidResponse`;
+    const loginID = `AdvancedSearch`;
     const ruleName = `BH Outpatient Rule ${Math.floor(1000 + Math.random() * 9000)}`;
 
     const ruleNameEdited = `${ruleName} - edit`;

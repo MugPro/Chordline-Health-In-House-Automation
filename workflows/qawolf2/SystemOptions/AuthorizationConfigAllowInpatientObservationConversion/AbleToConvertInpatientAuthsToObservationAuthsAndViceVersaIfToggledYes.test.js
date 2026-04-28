77f2a55e2f15ea68a2c9b0095c02abdbd09dff6a
Matas Backevicius <matas.backevicius+chord@chordline.com>
2026-04-28 13:19:04 -0700

    updated tests due to flakiness

--- workflows/qawolf2/SystemOptions/AuthorizationConfigAllowInpatientObservationConversion/AbleToConvertInpatientAuthsToObservationAuthsAndViceVersaIfToggledYes.test.js
+++ workflows/qawolf2/SystemOptions/AuthorizationConfigAllowInpatientObservationConversion/AbleToConvertInpatientAuthsToObservationAuthsAndViceVersaIfToggledYes.test.js
@@ -24,6 +24,201 @@ async function waitUntilLoaded2(page, options = {}) {
 
 
 
+async function NewCreateAuthorizationForMember(page, options = {}) {
+    // Constants
+    //const { dateFns } = npmImports;
+    const today = Date.now();
+    const lastFirstName = options.lastFirstName || `Ace, Clancy`;
+    const authorizationType = options.authorizationType || `Inpatient`;
+    const patientStatus = options.patientStatus || `Admitted`;
+    /*const admitDate =
+        options.admitDate || dateFns.format(today, "MM dd yyyy hh mm ss aa");*/
+
+    const admitDate =
+        options.admitDate || format(today, "MM dd yyyy hh mm ss aa");
+
+    const authStatus = options.authStatus || `In Progress`;
+    const team = options.team || `Case Team`;
+    const reviewer = options.reviewer;
+    let worklogActivityDate = `No worklog appeared please check system configurations > Worklogs`;
+
+    // Navigate to Home
+    await page.getByText(`Home`, { exact: true }).click();
+
+    // Navigate to the Members tab
+    await page.locator(`#home-tabs-tab-4`).getByText(`Members`).click();
+
+    // Search for a member
+    await page.getByRole(`textbox`, { name: `Search...` }).fill(lastFirstName);
+    await page.keyboard.press("Enter");
+
+    // Select the member to open member page
+    await page.getByRole(`gridcell`, { name: lastFirstName }).dblclick();
+    //await waitUntilLoaded(page);
+
+    // Click the "Authorizations" tab
+    await page.locator(`#authorizations-menu`).click();
+
+    // Hover over "+ Authorization" button and select option >> Inpatient
+    await page
+        .getByRole(`button`)
+        .filter({ hasText: `Authorization Inpatient` })
+        .hover();
+    await page
+        .getByLabel(lastFirstName)
+        .getByText(authorizationType, { exact: true })
+        .click();
+
+
+
+    if (authorizationType === "Inpatient" || authorizationType === "Observation") {
+        // Fill in the inpatient status and select the option
+        await page
+            .locator(`input[name="aush_inpatient_status_id__1_input"]`)
+            .fill(patientStatus);
+        await page
+            .getByRole(`option`, { name: patientStatus })
+            .locator(`span`)
+            .click();
+    }
+
+    if (authorizationType === "Inpatient") {
+        // Fill in the "Admit Date:"
+        await page.locator(`#aush_admit_date__1`).click();
+        await page.locator(`#aush_admit_date__1`).clear();
+        await page.locator(`#aush_admit_date__1`).pressSequentially(admitDate);
+    }
+
+    // Fill in "Auth Status:" and select option
+    await page.getByRole(`button`, { name: `` }).nth(2).click();
+    await page.locator(`input[name="aush_status_id__1_input"]`).clear();
+
+    //await waitUntilLoaded(page);
+
+    await page.locator(`input[name="aush_status_id__1_input"]`).fill(authStatus);
+    await page.getByRole(`option`, { name: authStatus }).locator(`span`).click();
+
+    // Fill in the "Summary > Team" section and select option
+    await page.locator(`input[name="auth_team_reference_id_input"]`).fill(team);
+    await page.getByRole(`option`, { name: team }).click();
+
+    if (authorizationType === "Outpatient" || authorizationType === "Observation") {
+        await page
+            .locator(`input[name="auth_reviewer_user_id_input"]`)
+            .fill(reviewer);
+        await page
+            .getByRole(`option`, { name: `${reviewer}` })
+            .locator(`span`)
+            .click();
+    }
+
+    // TODO: Need to make this dynamic possibly
+    // Select Provider
+    await page
+        .locator(`[name="auth_provider_1_site_id"] ~ button[title="Lookup"]`)
+        .click();
+
+
+
+
+    // Check In and Out of Network boxes
+    await page.getByRole(`checkbox`, { name: `Out of Network` }).check();
+    await page.getByRole(`checkbox`, { name: `In Network` }).check();
+
+    // Search for the provider
+    await page
+        .getByRole(`textbox`, { name: `Search...` })
+        .fill(`St. Catherine's Hospital`);
+    await page
+        .getByRole(`dialog`, { name: `Lookup` })
+        .locator(`#lookup-search-button`)
+        .click();
+    await page
+        .getByRole(`gridcell`, { name: `St. Catherine's Hospital` })
+        .first()
+        .click();
+    await page.getByRole(`button`, { name: `Select`, exact: true }).click();
+
+    // TODO: Need to make this dynamic possibly
+    // Select Admitting Provider
+    try {
+        await page
+            .locator(`[name="auth_provider_2_site_id"] ~ button[title="Lookup"]`)
+            .click();
+    } catch {
+        // Handle the "Notification" pop up for admitting hospital duplicate
+        await expect(page.getByText(`Notification`, { exact: true })).toBeVisible({
+        });
+        await page.getByRole(`button`, { name: `Okay` }).click();
+
+        await page
+            .locator(`[name="auth_provider_2_site_id"] ~ button[title="Lookup"]`)
+            .click();
+    }
+
+
+
+    // Try to dimiss the notification
+    try{
+        await page.getByText(`Notification`, { exact: true}).waitFor();
+        await page.getByRole(`button`, { name: `Okay` }).click();
+    }catch{
+        console.log("Notification did not appear.");
+    }
+
+
+
+    // Check In and Out of Network boxes
+    await page.getByRole(`checkbox`, { name: `Out of Network` }).check();
+
+
+
+    await page.getByRole(`checkbox`, { name: `In Network` }).check();
+
+
+    // Search for the provider
+    await page
+        .getByRole(`textbox`, { name: `Search...` })
+        .fill(`St. Catherine's Hospital`);
+    await page
+        .getByRole(`dialog`, { name: `Lookup` })
+        .locator(`#lookup-search-button`)
+        .click();
+    await page
+        .getByRole(`gridcell`, { name: `St. Catherine's Hospital` })
+        .first()
+        .click();
+
+
+    await page.getByRole(`button`, { name: `Select`, exact: true }).click();
+
+    // Click the "Save" button
+    await page.getByRole(`button`, { name: ` Save` }).click();
+    //await waitUntilLoaded(page);
+
+    try {
+        await expect(page.getByText(`New Work Log`)).toBeVisible();
+        // Grab the work activity date of the work log
+        worklogActivityDate = await page
+            .locator(`#work_activity_date`)
+            .evaluate((e) => e.value);
+        await page.getByRole(`button`, { name: ` Save and Close` }).click();
+    } catch {
+        await expect(page.getByText(`New Work Log`)).not.toBeVisible();
+    }
+    await waitUntilLoaded(page);
+
+    // Grab the "Auth #"
+    await page.locator(`#form-header .headerLabel`).waitFor();
+    const authNum = await page.locator(`#form-header .headerLabel`).innerText();
+
+    // Return the authNum for cleanup later
+    return { authNum, worklogActivityDate };
+}
+
+
+
+
 
 
 
@@ -49,6 +244,7 @@ test('AbleToConvertObservationAuthsIntoInpatientAuthsAndViceVera', async () => {
     const { page, browser } = await logIn({
         url: process.env.DEFAULT_URL_2,
         loginID,
+        slowMo: 1000,
     });
 
     //--------------------------------
@@ -84,12 +280,12 @@ test('AbleToConvertObservationAuthsIntoInpatientAuthsAndViceVera', async () => {
     }
 
     await page.getByRole(`button`, { name: `Save and Close` }).click();
-    await waitUntilLoaded2(page);
+    //await waitUntilLoaded2(page);
 
     //--------------------------------
     // Create Inpatient Authorization
     //--------------------------------
-    const { authNum } = await createAuthorizationForMember(page, {
+    const { authNum } = await NewCreateAuthorizationForMember(page, {
         lastFirstName,
         authorizationType,
         patientStatus,
@@ -123,7 +319,7 @@ test('AbleToConvertObservationAuthsIntoInpatientAuthsAndViceVera', async () => {
 
     await page.getByText(`Convert to Observation`).click();
 
-    await waitUntilLoaded2(page);
+    //await waitUntilLoaded2(page);
 
 
     await expect(page.getByText(`Auth Conversion`)).toBeVisible();
@@ -196,7 +392,7 @@ test('AbleToConvertObservationAuthsIntoInpatientAuthsAndViceVera', async () => {
 
 
 
-    await waitUntilLoaded2(page);
+    //await waitUntilLoaded2(page);
 
 
 
@@ -208,7 +404,7 @@ test('AbleToConvertObservationAuthsIntoInpatientAuthsAndViceVera', async () => {
 
     await page.getByRole(`button`, { name: ` Save and Close` }).click();
 
-    await waitUntilLoaded2(page);
+    //await waitUntilLoaded2(page);
 
 
 
@@ -216,7 +412,7 @@ test('AbleToConvertObservationAuthsIntoInpatientAuthsAndViceVera', async () => {
 
 
     await expect(page.getByText(`New Work Log`)).toBeVisible();
-    await waitUntilLoaded2(page);
+    //await waitUntilLoaded2(page);
     await page.getByRole(`button`, { name: ` Save and Close` }).click();
 
 
@@ -264,96 +460,4 @@ test('AbleToConvertObservationAuthsIntoInpatientAuthsAndViceVera', async () => {
 
 
 
-
-
-
-
-
-/*
-    await page.locator(`input[name="auth_request_date"]`).fill(authReqDate);
-    await page.locator(`#auth_admit_date`).fill(authReqDate);
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*
-
-
-    //--------------------------------
-    // Assert Inpatient Page
-    //--------------------------------
-    await expect(
-        page.locator(`#inpatientstatus-anchor div`).filter({ hasText: `Inpatient Status` })
-    ).toBeVisible();
-
-    const inpAuthNum = await page.locator(`#form-header .headerLabel`).innerText();
-
-    //--------------------------------
-    // Navigate back to Authorizations grid
-    //--------------------------------
-    await page.getByRole(`button`, { name: ` All Auths` }).click();
-    await waitUntilLoaded(page);
-
-    //--------------------------------
-    // Assertions in Grid
-    //--------------------------------
-    await expect(async () => {
-        await page.getByText(`Auth ID`, { exact: true }).click();
-
-        await expect(
-            page.locator(
-                `table tbody tr:has(td span[id="id"]:text-is("${authNum.replace("Inpatient Auth #", "")}"))`
-            )
-        ).toContainText("Closed");
-    }).toPass({ timeout: 30000 });
-
-    await expect(
-        page.locator(
-            `table tbody tr:has(td span[id="id"]:text-is("${obsAuthNum.replace("Observation Auth #", "")}"))`
-        )
-    ).toContainText("Closed");
-
-    await expect(
-        page.locator(
-            `table tbody tr:has(td span[id="id"]:text-is("${inpAuthNum.replace("Inpatient Auth #", "")}"))`
-        )
-    ).toContainText("Admitted");
-
-    //--------------------------------
-    // Cleanup (post-test)
-    //--------------------------------
-    try {
-        await cleanupTabOnMembersPage(page, {
-            tab: "Authorizations",
-            gridId: '[id="authorizations-grid"]',
-            memberName: lastFirstName,
-            loginID,
-            onScreen: true,
-        });
-    } catch (e) {
-        await reportCleanupFailed({
-            dedupKey: "cleanupTabOnMembersPage",
-            errorMsg: e.message,
-        });
-    }
-
-    await browser.close();
-});
-
-     */
-
-    //await browser.close();
 });
